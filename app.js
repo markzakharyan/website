81c1b120514917f8b44d166f3affd97cad5880ec
Mark Zakharyan <markzakharyan@ucsb.edu>
2024-06-23 11:52:52 -0700

    quick fix the secure cookie bug

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
     secret: process.env.SESSION_SECRET || "your_secret_key",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === "production" },
+    cookie: { secure: false },
   })
 );
 
