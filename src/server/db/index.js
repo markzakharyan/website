d91d8d4e0a3ea03aa8b28b2c9a58c0b901468ef9
Mark Zakharyan <markzakharyan@ucsb.edu>
2024-07-07 18:06:17 -0700

    added private url for database security and speed

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.NODE_ENV === 'production' ? DATABASE_PRIVATE_URL : process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
